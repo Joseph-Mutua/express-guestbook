b41ea9b514dd7b8414f36582038557cdfafcbf5d
Joseph Mutua <Mutuaj793@gmail.com>
2020-05-25 22:00:06 +0300

    Add controller actions for guestbook

--- app.js
+++ app.js
@@ -17,3 +17,36 @@ app.use(logger("dev"));
 
 //Populates a variable called req.body if the user is submitting a form
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//Rendering the site root
+app.get("/", function (request, response) {
+  response.render("index");
+});
+
+//Render page for creating new entry
+app.get("/new-entry", function (request, response) {
+  response.render("new-entry");
+});
+
+//Handle post requests to new-entry URL
+app.post("/new-entry", function (request, response) {
+  if (!request.body.title || !request.body.body) {
+    response.status(400).send("Entries must have a title and a body");
+  }
+  entries.push({
+    title: request.body.title,
+    content: request.body.body,
+    published: new Date(),
+  });
+  response.redirect("/");
+});
+
+//Render 404 pagefor unknown page
+app.use(function (request, response) {
+  respinse.status(404).render("404");
+});
+
+//Start server on port 3000
+http.createServer(app).listen(3000, function () {
+  console.log("Guestbook app started on port 3000");
+});
